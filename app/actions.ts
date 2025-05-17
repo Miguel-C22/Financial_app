07bb00ce4cda6bf9ca1d222bea7d359733056957
Miguel-C22 <miguel.ganoza@treez.io>
2025-05-17 16:15:25 -0500

    redirect

--- app/actions.ts
+++ app/actions.ts
@@ -23,7 +23,7 @@ export const signUpAction = async (formData: FormData) => {
     email,
     password,
     options: {
-      emailRedirectTo: `${origin}/home`,
+      emailRedirectTo: `${origin}/auth/callback?redirect_to=/home`,
     },
   });
 
